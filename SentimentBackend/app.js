a597418735b7bb83a57b8b7d9f225e892f83f658
Pranav Kumar <prkumar@SFMBPPKUMAR.local>
2016-08-11 16:07:52 -0700

    allow headers for core

--- SentimentBackend/app.js
+++ SentimentBackend/app.js
@@ -5,6 +5,28 @@ var PORT = process.env.PORT || 3000
 
 var articles = require('./routes/articles');
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(allowCrossDomain);
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 app.use('/api/articles', articles);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
