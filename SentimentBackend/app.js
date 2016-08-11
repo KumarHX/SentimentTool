73714cbe18387a2b6359bd35b8b452f57ea94a10
Pranav Kumar <prkumar@SFMBPPKUMAR.local>
2016-08-11 15:21:25 -0700

    mor

--- SentimentBackend/app.js
+++ SentimentBackend/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var PORT = process.env.PORT || 3000
 
 var articles = require('./routes/articles');
 
@@ -8,7 +9,7 @@ app.use('/api/articles', articles);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
   console.log('Example app listening on port 3000!');
 });
 
