4b68f2c22f8efc07130e425e3ac790e1de16e891
Corey McCourt <cmccourt@SFMBPCMCCOURT.local>
2016-08-15 14:45:04 -0700

    Update js with ajax call and css

--- SentimentFrontend/js/logic.js
+++ SentimentFrontend/js/logic.js
@@ -1,12 +1,15 @@
 
 $(function(){
   var ctx = $('.lineChart'),
-  endpointHost = 'https://sentiment-tool.herokuapp.com/api/articles/',
-  fakeData = {
+  // endpointHost = 'https://sentiment-tool.herokuapp.com/api/articles/',
+  endpointHost = 'http://apis.ign.com/slotter/v3/slotters/loot-hunt/_published',
+
+  data = {
     objectTitle: 'No Man\'s sky',
-    objectCoverArt: "PICTURE",
+    objectCoverArt: "http://assets1.ignimgs.com/2014/06/16/nomanssky1jpg-b688bd.jpg",
     objectOverallGood: 344,
     objectOverallBad: 201,
+
     dataPoints: [
       {
         day: 'Monday',
@@ -40,88 +43,104 @@ $(function(){
     ]
   };
 
+    var build = function (data) {
+        $('.boxArt').attr('src', data.objectCoverArt);
+        $('.objectTitle').html(data.objectTitle);
+        $('.statItem.good p').html(data.objectOverallGood);
+        $('.statItem.bad p').html(data.objectOverallBad);
 
 
-    var data = {
-        labels: fakeData.dataPoints.map(function(point){
-          return point.day;
-        }),
-        datasets: [
-            {
-                label: 'Sentiment Data',
-                fill: false,
-                lineTension: 0.1,
-                backgroundColor: "#FFF",
-                fill: false,
-                borderColor: "#BF1313",
-                borderCapStyle: 'butt',
-                borderDash: [],
-                borderDashOffset: 0.0,
-                borderJoinStyle: 'miter',
-                pointBorderColor: "rgba(75,192,192,1)",
-                pointBackgroundColor: "#BF1313",
-                pointBorderWidth: 10,
-                pointHoverRadius: 5,
-                pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                pointHoverBorderColor: "rgba(220,220,220,1)",
-                pointHoverBorderWidth: 2,
-                pointRadius: 1,
-                pointHitRadius: 10,
-                data: fakeData.dataPoints.map(function(point){
-                    return point.overall;
-                }),
-                spanGaps: false
-            }
-        ]
-    }
-  var myLineChart = new Chart(ctx, {
-      type: 'line',
-      data: data
-  });
+        var chartData = {
+            labels: data.dataPoints.map(function(point){
+              return point.day;
+            }),
+            datasets: [
+                {
+                    label: 'Sentiment Data',
+                    fill: false,
+                    lineTension: 0.1,
+                    backgroundColor: "#FFF",
+                    fill: false,
+                    borderColor: "#BF1313",
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: "rgba(75,192,192,1)",
+                    pointBackgroundColor: "#BF1313",
+                    pointBorderWidth: 10,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: data.dataPoints.map(function(point){
+                        return point.overall;
+                    }),
+                    spanGaps: false
+                }
+            ]
+        }
+      var myLineChart = new Chart(ctx, {
+          type: 'line',
+          data: chartData
+      });
 
-  $('.lineChart').on('click', function(e){
-    var index = myLineChart.getElementAtEvent(e)[0]._index,
-        articleArray = fakeData.dataPoints[index].articles,
-        $articleCards = $('.articleCard');
+      $('.lineChart').on('click', function(e){
+        var index = myLineChart.getElementAtEvent(e)[0]._index,
+            articleArray = data.dataPoints[index].articles,
+            $articleCards = $('.articleCard');
 
-        $articleCards.each(function(index){
-          $(this).fadeOut(function(){
-            $(this).remove();
-          })
+            $articleCards.each(function(index){
+              $(this).fadeOut(function(){
+                $(this).remove();
+              })
+            })
+
+        articleArray.forEach(function(articleObj, ind){
+          var name = "No Man's Sky gets Game of the Year",
+              sentiment = "75",
+              votes = "450",
+              imageUrl = "images/sky.jpg",
+              articleUrl = 'http://www.google.com',
+              trendClass = sentiment >= 50 ? 'happy' : 'sad',
+              element= '<div class="articleCard"><a href="' + articleUrl +'" target="#blank">\
+                        <h3>' + name + '</h3>\
+                            <div class="article-stuff-container">\
+                                <div class="rating ' + trendClass + '"><p class="percent">' + sentiment + '%</p><p class="numVotes">' + votes + ' votes</p></div>\
+                             </div>\
+                            </a>\
+                          </div>';
+          setTimeout(function(){
+            $('.articleCard-container').append(element)
+            $('.articleCard:last-child').css('background-image', 'url("images/black.png"), url(' + imageUrl + ')')
+
+          }, (ind + 1) * 500)
         })
 
-    articleArray.forEach(function(articleObj, ind){
-      var name = "No Man's Sky gets Game of the Year",
-          sentiment = "75",
-          votes = "450",
-          imageUrl = "images/sky.jpg",
-          trendClass = sentiment >= 50 ? 'happy' : 'sad',
-          element= '<div class="articleCard"><a href="http://www.google.com" target="#blank">\
-                    <h3>' + name + '</h3>\
-                        <div class="article-stuff-container">\
-                            <div class="rating ' + trendClass + '"><p class="percent">' + sentiment + '%</p><p class="numVotes">' + votes + ' votes</p></div>\
-                         </div>\
-                        </a>\
-                      </div>';
-      setTimeout(function(){
-        $('.articleCard-container').append(element)
-        $('.articleCard:last-child').css('background-image', 'url("images/black.png"), url(' + imageUrl + ')').fadeIn(ind*500)
+      })
+    }
 
-      }, 500)
-    })
 
-  })
 
-  var requestDataPoints = function(){
+  var initChart = function(){
       $.ajax({
-        url: endpoint,
+        url: endpointHost,
         method: 'GET',
+        dataType: 'jsonp',
+        data: {
+          format: 'js'
+        },
         success: function(response){
           console.log(response);
+          build(data);
         },
         error: function(a,b,c){
           console.log(a,b,c);
         }
       })
     }
+
+    initChart();
 })
